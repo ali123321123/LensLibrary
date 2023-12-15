a50f21ad957e7a07df235af68f0befa630c46e70
alzeda <ALI.ZEDAN@NORGESGRUPPEN.NO>
2023-12-15 16:40:24 +0100

    Solved part2

--- LensLibrary2/Program.cs
+++ LensLibrary2/Program.cs
@@ -41,7 +41,28 @@ public class Solution
         }
         return num;
     }
+    public static void HashMap(string label, string operation, string focalLength, Dictionary<int, List<(string, int)>> boxes)
+    {
+        int boxNum = HashAlgorithm(label);
+        if (!boxes.ContainsKey(boxNum))
+        {
+            boxes[boxNum] = new List<(string, int)>();
+        }
 
+        if (operation == "=")
+        {
+            var lens = (label, int.Parse(focalLength));
+            boxes[boxNum] = boxes[boxNum].Select(l => l.Item1 == label ? lens : l).ToList();
+            if (boxes[boxNum].All(l => l.Item1 != label))
+            {
+                boxes[boxNum].Add(lens);
+            }
+        }
+        else if (operation == "-")
+        {
+            boxes[boxNum] = boxes[boxNum].Where(l => l.Item1 != label).ToList();
+        }
+    }
  
 
     public static void Main(string[] args)
@@ -51,6 +72,19 @@ public class Solution
         var part1Result = inputList.Select(HashAlgorithm).Sum();
         Console.WriteLine("Part 1 = " + part1Result);
 
+        
+        // Part 2
+        var labelOperations = ReadLabelOperationLens("../../../input.txt");
+        var boxes = new Dictionary<int, List<(string, int)>>();
+        foreach (var (label, operation, focalLength) in labelOperations)
+        {
+            HashMap(label, operation, focalLength, boxes);
+        }
+
+        int totalFocusingPower = boxes.Sum(box =>
+            box.Value.Select((lens, slotIndex) => (box.Key + 1) * (slotIndex + 1) * lens.Item2).Sum());
+
+        Console.WriteLine("Part 2 = " + totalFocusingPower);
      
     }
 }
